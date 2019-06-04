1bfc7b78458e1486ba57ae3b22b5d653bcbbadd6
Symmetry Games <symmetrygames@outlook.com>
2019-06-04 17:32:30 +0100

    .

---
+++ JavaScript/about.js
@@ -0,0 +1,38 @@
+
+
+var aText = new Array(
+    "I am a chemical engineer with a minor in mathematics. I am also a skilled developer that specialises in HTML5, CSS3, JavaScript, C# and ASP .NET MVC. I primarily focus on Front-End Web development and iOS and Android Game development using Unity3D. I am able to use the following frameworks to further my craft: Boostrap 4, JQuery , React .JS and MonoDevelop"
+    );
+    var iSpeed = 100; // time delay of print out
+    var iIndex = 0; // start printing array at this posision
+    var iArrLength = aText[0].length; // the length of the text array
+    var iScrollAt = 20; // start scrolling up at this many lines
+     
+    var iTextPos = 0; // initialise text position
+    var sContents = ''; // initialise contents variable
+    var iRow; // initialise current row
+     
+    function typewriter()
+    {
+     sContents =  ' ';
+     iRow = Math.max(0, iIndex-iScrollAt);
+     var destination = document.getElementById("bioh4");
+     
+     while ( iRow < iIndex ) {
+      sContents += aText[iRow++] + '<br />';
+     }
+     destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
+     if ( iTextPos++ == iArrLength ) {
+      iTextPos = 0;
+      iIndex++;
+      if ( iIndex != aText.length ) {
+       iArrLength = aText[iIndex].length;
+       setTimeout("typewriter()", 500);
+      }
+     } else {
+      setTimeout("typewriter()", iSpeed);
+     }
+    }
+    
+    
+    typewriter();
